6738720619a89fbce3c2b75850b0d4b829ea45e1
Moa Granberg <moa.granberg@hotmail.com>
2020-10-28 15:21:18 +0100

    Form posts searchQuery, when set do_search()

--- index.php
+++ index.php
@@ -3,22 +3,33 @@
 Plugin Name: The Pied Search Miracle
 */
 
-function do_search()
+function do_search($searchQuery)
 {
   global $wpdb;
   $table_name = $wpdb->prefix . "posts";
-  $results = $wpdb->get_results("SELECT * FROM $table_name WHERE post_content LIKE '%halloj%' AND post_content LIKE '%världen%'" );
+  $results = $wpdb->get_results("SELECT * FROM $table_name WHERE post_content LIKE '%$searchQuery%'");
   foreach ($results as $row) {
-    error_log("Permalink: " . get_permalink($row->ID) . " Post title: " . $row->post_title . " Post content: " .$row->post_content);
+    error_log("Permalink: " . get_permalink($row->ID) . " Post title: " . $row->post_title . " Post content: " . $row->post_content);
   }
 }
 
-function get_content($content) {
+function get_content($content)
+{
   $searchBtn = "<a href='index.php?gogosearch=true'><button>Search...</button></a>";
   $searchInputField = "";
 
-  if(isset($_GET["gogosearch"])) {
-    $searchInputField = "<input type='text' placeholder='search...'></input>";
+  if (isset($_GET["gogosearch"])) {
+    $searchBtn = '';
+    $searchInputField = "
+    <form action='' method='POST'>
+      <input type='text' placeholder='search...' name='searchQuery' />
+      <input type='submit'/>
+    </form>";
+
+    if (isset($_POST['searchQuery'])) {
+      $searchQuery = $_POST['searchQuery'];
+      do_search($searchQuery);
+    }
   }
 
   return $content . $searchBtn . $searchInputField;
